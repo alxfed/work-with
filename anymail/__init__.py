5c944fa3d073b231fd2e15257a8acc7f12da4094
alxfed <alxfed@gmail.com>
2019-11-13 08:55:43 -0600

    anymail as a package

--- anymail/__init__.py
+++ anymail/__init__.py
@@ -5,7 +5,16 @@ from . import find, verify
 
 
 def main():
+    print('You have launched anymail __init__.py as main')
     return
+
+
+key_file = open(AUTHORIZATION_KEY_FILE, 'r')
+anymail_api_key = key_file.read()
+key_file.close()
+
+# use this header with all the API calls
+headers = {'X-Api-Key': anymail_api_key}
 
 
 if __name__ == '__main__':
