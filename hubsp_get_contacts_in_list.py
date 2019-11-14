e2ecc7427ddcb5232214c644050d1d08e5981069
alxfed <alxfed@gmail.com>
2019-11-14 15:00:52 -0600

    hubspot contacts in a list

--- hubsp_get_contacts_in_list.py
+++ hubsp_get_contacts_in_list.py
@@ -8,7 +8,7 @@ import pandas as pd
 def main():
     contacts_list_file = '/home/alxfed/archive/downloaded_list.csv'
     cont_list = hubspot.lists.get_all_contacts_in('260')
-    output = pd.DataFrame(cont_list)
+    output = pd.DataFrame(cont_list, dtype=object)
     output.to_csv(contacts_list_file, index=False)
     return
 
