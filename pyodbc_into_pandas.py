1054d4a3335465b54fac9d9fbb7ed5d9ad640415
alxfed <alxfed@gmail.com>
2019-11-17 17:54:47 -0600

    python odbc

--- pyodbc_into_pandas.py
+++ pyodbc_into_pandas.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 import string
 import odbc
+import sqlalchemy as sqlalc
 
 
 def main():
@@ -16,6 +17,12 @@ def main():
     data = pd.read_sql(SQL, connection)
     '''
     connection = odbc.dbase.connection_with(name_of_dsn)
+    SQL = 'select * from rand'
+    data = pd.read_sql_query(SQL, connection)
+    connection.close()
+
+    other_conn = sqlalc.create_engine('sqlite:///home/alxfed/dbase/home.sqlite')
+    other = pd.read_sql(SQL, con=other_conn)
     lett = list(string.ascii_lowercase)
     df = pd.DataFrame(np.random.choice(lett, size=(3, 3)), index=['a', 'b', 'c'], columns=[0, 1, 2])
     res = df.to_sql(name='rand', con=connection, if_exists='replace')
