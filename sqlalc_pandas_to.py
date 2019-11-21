99554bb21b2be9185e941cd6a24c1c04d3b65cd4
alxfed <alxfed@gmail.com>
2019-11-21 10:09:35 -0600

    odbc and sqlalc

--- sqlalc_pandas_to.py
+++ sqlalc_pandas_to.py
@@ -7,13 +7,14 @@ import sqlalchemy as sqlalc
 
 def main():
     conn = sqlalc.create_engine('sqlite:////home/alxfed/dbase/home.sqlite')
-
+    # just for the purpose of testing
     data = pd.read_sql_table(table_name='companies', con=conn) # works
     id = data['companyId'][0]
     deleted = data['isDeleted'][0]
     name = data['name'][0]
     phone = data['phone'][0]
 
+    # the thing itself
     data.to_sql(name='companies', con=conn, if_exists='replace', index=False)
     print('ok')
     return
