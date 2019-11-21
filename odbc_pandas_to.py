4bebda5ea16b73df995acdad2cf6e30e9e12430e
alxfed <alxfed@gmail.com>
2019-11-21 09:10:02 -0600

    odbc and sqlalc

---
+++ odbc_pandas_to.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+import pandas as pd
+import numpy as np
+import string
+import odbc
+import sqlalchemy as sqlalc
+
+
+def main():
+    name_of_dsn = 'home'
+    connection = odbc.dbase.connection_with(name_of_dsn)
+    SQL = 'select * from companies'
+    data = pd.read_sql_query(SQL, connection)
+    id = data['companyId'][0]
+    deleted = data['isDeleted'][0]
+    name = data['name'][0]
+    phone = data['phone'][0]
+
+    another_conn = odbc.dbase.connection_with('center')
+    data.to_sql('companies', con=another_conn, if_exists='replace')
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
