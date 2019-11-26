88eb3cebab0f9852fce7b2d3e61f56d2adac4d9e
alxfed <alxfed@gmail.com>
2019-11-26 06:49:28 -0600

    detrack

---
+++ detrack/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""https://www.detrack.com/api-documentation/
+"""
+import requests
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
