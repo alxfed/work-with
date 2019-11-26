b32b004df1cd2db326d40a55562cb48cc775ab66
alxfed <alxfed@gmail.com>
2019-11-26 11:22:41 -0600

    scrape into sqlite

--- data/pipelines.py
+++ data/pipelines.py
@@ -4,8 +4,32 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-
+import odbc
 
 class DataPipeline(object):
+
+    database_name = 'data'
+    table_name = 'scraped_data'
+
+    def open_spider(self, spider):
+        self.cnxn = odbc.dbase.connection_with(self.database_name)
+        self.curs = self.cnxn.cursor()
+        self.curs.execute('drop table if exists scraped_data')
+        self.curs.execute('''create table ?(
+                            one text,
+                            two text,
+                            three text''', self.table_name)
+
+    def close_spider(self, spider):
+        self.cnxn.close()
+
+    def store(self, item):
+        self.curs.execute("""insert into scraped_data values(?, ?, ?)""",
+                              item['one'],
+                              item['two'],
+                              item['three'])
+
     def process_item(self, item, spider):
+        self.store(item)
+        print('Pipeline has stored an item to database')
         return item
--- data/settings.py
+++ data/settings.py
@@ -19,10 +19,10 @@ NEWSPIDER_MODULE = 'data.spiders'
 #USER_AGENT = 'data (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 1
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
@@ -64,9 +64,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'data.pipelines.DataPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'data.pipelines.DataPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
