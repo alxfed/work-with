b798e67b6d4073fa256222b40f9af40080002d0b
alxfed <alxfed@gmail.com>
2019-12-09 07:07:04 -0600

    sorting

---
+++ sorting/companies.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+import sqlalchemy
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
---
+++ sorting/constants.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+
+SOURCE_DATABASE = ''
+TARGET_DATABASE = ''
+
+
+def main():
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
