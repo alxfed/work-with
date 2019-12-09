d74a45cf600601dd792d96d4c8cac3374b855c58
alxfed <alxfed@gmail.com>
2019-12-09 07:37:21 -0600

    licensed general contractors

--- sorting/constants.py
+++ sorting/constants.py
@@ -5,6 +5,8 @@ HOME_DATABASE_URI = 'sqlite:////home/alxfed/dbase/home.sqlite'
 SOURCE_DATABASE_URI = 'sqlite:////home/alxfed/dbase/home.sqlite'
 TARGET_DATABASE_URI = 'sqlite:////home/alxfed/dbase/firstbase.sqlite'
 
+LICENSED_GENERAL_CONTRACTORS_TABLE = 'all_licensed_general_contractors'
+
 
 def main():
     print('You have launched constants as main')
