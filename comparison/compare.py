0a49729decc9ccce6c0f78e24c6ba030c40a9eda
alxfed <alxfed@gmail.com>
2019-12-10 15:21:13 -0600

    comparison

--- comparison/compare.py
+++ comparison/compare.py
@@ -1,13 +1,27 @@
 import re
-from .constants import *
+# from .constants import *
 
 
 def first_word(name):
-
+    length = len(name)
+    no_spaces = length - len(name.replace(' ', ''))
+    no_periods = length - len(name.replace('.', ''))
+    no_commas = length - len(name.replace(',', ''))
+    no_dashes = length - len(name.replace('-', ''))
+    and_sign = length - len(name.replace('&', ''))
+    one, sep, rest = name.partition(' ')
+    # if len(one) < 3:
+    #
     return first_word
 
 
 def main():
+    name1 = 'L & S Lighting Corp'
+    name2 = 'S. & P. Tax Solutions'
+    name3 = 'I.B. Quality Cabinets'
+    name4 = 'C&H Kitchen Cabinet Gallery'
+    name5 = 'D.O.M.'
+    first = first_word(name2)
     return
 
 
