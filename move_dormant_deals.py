7f8786c1377b2bc9ea1d1809b5d995d3b6fe0ab8
alxfed <alxfed@gmail.com>
2019-12-22 18:27:56 -0600

    finally moving to dormant

--- move_dormant_deals.py
+++ move_dormant_deals.py
@@ -27,34 +27,18 @@ def main():
 
     deals_list = deals['dealId'].to_list()
 
-    # for indx, row in no_close_date_follow_up.iterrows():
-    #     line = {}
-    #     deal_properties = hubspot.deals.get_a_deal(row['dealId'])['properties']
-    #     source = deal_properties['dealstage']['source']
-    #     if deal_properties['dealstage']['source'] == 'BATCH_UPDATE':
-    #         # print('here you are') # 155182970482
-    #         sing = deal_properties['dealstage']['timestamp']
-    #         dif = abs(sing - hs_date)
-    #         if dif < 10000000:
-    #             line['dealId'] = row['dealId']
-    #             line['timestamp'] = str(sing)
-    #             datm = pd.to_datetime(sing, unit='ms')
-    #             print("deal is", row['dealId'], "  this timestamp is:", datm)
-    #             line_list.append(line)
-    #
-    # those_deals = pd.DataFrame(line_list)
-    # # conn = sqlalc.create_engine(sorting.HOME_DATABASE_URI)
-    # those_deals.to_sql(name=sorting.THOSE_DEALS_TABLE, con=conn, if_exists='replace', index=False)
     params = {'pipeline': '1243605', 'dealstage': '1243606'}
+
     chunk_length = 100
     chunk_list = [deals_list[i:i + chunk_length] for i in range(0, len(deals_list), chunk_length)]
+
     it = 0
     for chunk in chunk_list:
         response = hubspot.deals.batch_update_deals(chunk, params)
         it += 1
         print('Updated', it)
-    print('ok')
 
+    print('ok')
     return
 
 
