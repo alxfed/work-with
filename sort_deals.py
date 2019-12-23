6497528adced070d07f9277f9115544479999dd7
alxfed <alxfed@gmail.com>
2019-12-23 06:51:13 -0600

    create update

--- sort_deals.py
+++ sort_deals.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import sorting
 import sqlalchemy as sqlalc
+import datetime as dt
 
 
 def main():
@@ -14,52 +15,17 @@ def main():
     data = pd.read_sql_table(
         table_name=sorting.DEALS_TABLE, con=conn_source)
 
-    fresh_deals = data[data[]]
+    start_date  = dt.datetime(year=2019, month=12, day=1, hour=0, minute=0, second=0)
+    end_date    = dt.datetime(year=2019, month=12, day=24, hour=0, minute=0, second=0)
 
-    old_companies_permits = pd.DataFrame()
-    new_companies = pd.DataFrame()
-    not_found_companies = pd.DataFrame()
-    seen_new_companies = set()
-    seen_not_found = set()
+    fresh_deals = data[(data['createdate'] > start_date) & (data['createdate'] < end_date)]
 
-    for index, this_permit in data.iterrows():
-        # Debug for particular company
-        # debug_company_name = 'Mk Construction'
-        # debug_company = this_permit['name']
-        # if debug_company_name in debug_company:
-        #     print('ok')
-        # Debug for particular company
-        permit_to_add, company_to_add, not_found = sorting.companies.compare_with_companies_and_reference(
-            this_permit, companies, licensed_gen_contractors)
-        if not company_to_add.empty:
-            if not company_to_add['company_name'].values[0] in seen_new_companies:
-                new_companies = new_companies.append(company_to_add, ignore_index=True)
-                seen_new_companies.add(company_to_add['company_name'].values[0])
-        if not permit_to_add.empty:
-            old_companies_permits = old_companies_permits.append(permit_to_add, ignore_index=True)
-        if not not_found.empty:
-            if not not_found['name'].values[0] in seen_not_found:
-                not_found_companies = not_found_companies.append(not_found, ignore_index=True)
-                seen_not_found.add(not_found['name'].values[0])
-
-
-    # upload them to the firstbase database
-    conn_target = sqlalc.create_engine(sorting.PREP_DATABASE_URI)
-    old_companies_permits.to_sql(
-        name=sorting.OLD_COMPANIES_PERMITS_TABLE,
-        con=conn_target, if_exists='replace', index=False)
-
-    # unique
-    not_found_companies.to_sql(
-        name=sorting.NOT_FOUND_GENERAL_CONTRACTORS_TABLE,
-        con=conn_target, if_exists='replace', index=False)
-
-    conn_interm = sqlalc.create_engine(sorting.INTERM_DATABASE_URI)
-    if not new_companies.empty:
-        new_companies.to_sql(
-            name=sorting.NEW_COMPANIES_TABLE,
-            con=conn_interm, if_exists='replace', index=False)
-
+    if not fresh_deals.empty:
+        fresh_deals.to_sql(
+            name=sorting.FRESH_DEALS_TABLE,
+            con=conn_target, if_exists='replace', index=False)
+    else:
+        print('No deals created in this time frame')
     return
 
 
--- sorting/summary.py
+++ sorting/summary.py
@@ -2,12 +2,133 @@
 """...
 """
 from . import constants
+import pandas as pd
+import sqlalchemy as sqlalc
+import hubspot
+import datetime as dt
 
 
-def main():
-    return
+class SummaryNote(object):
+    ownerId = '40202623' # Marfa Cabinets Data robot
 
+    def __init__(self, **kwargs):
+        if 'engagementId' in kwargs.keys(): self.engagementId = kwargs['engagementId']
+        else: self.engagementId = ''
+        if 'companyId' in kwargs.keys(): self.companyId = kwargs['companyId']
+        else: self.companyId = ''
+        if 'deal_list' in kwargs.keys(): self.deal_list = kwargs['deal_list']
+        else: self.deal_list = []
+        if 'content' in kwargs.keys(): self.content = kwargs['content']
+        else: self.content = ''
+        if 'hs_timestamp' in kwargs.keys(): self.hs_timestamp = kwargs['hs_timestamp']
+        else: self.hs_timestamp = str(int(1000 * dt.datetime.now().timestamp()))
+        self.ready = False
 
-if __name__ == '__main__':
-    main()
-    print('main - done')
+    def __del__(self): # just in case I will want to add the deletion of note here
+        pass
+
+    def read_in(self, engagementId: str):
+        # readin engagement
+        result = ''
+        if result:
+            return True
+        else:
+            return False
+
+    def add_content(self, **kwargs):
+        self.content = ''
+        return True
+
+    def verify_content(self):
+        # verification
+        self.ready = True
+        return self.ready
+
+    def create(self):
+        if self.ready:
+            params = {'ownerId': SummaryNote.ownerId,
+                      'timestamp': self.hs_timestamp,
+                      'companyId': self.companyId,
+                      'dealIds': self.deal_list,
+                      'note': self.content}
+            cre = hubspot.engagements.create_engagement_note(params)
+            if cre:
+                created_note = cre['engagement']
+                self.engagementId = created_note['id']
+                result = hubspot.companies.update_company(self.companyId, {'summary_note': self.engagementId,
+                                     'summary_note_date': self.hs_timestamp})
+                if result:
+                    print('Created Summary Note: ', self.engagementId)
+            pass
+        else:
+            print('Is not ready')
+            return False
+
+    def update(self, timestamp):
+        if self.ready:
+            params = {'timestamp': timestamp, 'note': self.content}
+            res = hubspot.engagements.update_an_engagement(self.engagementId, params)
+            # if res:
+            #     print('updated the note ', inspection_note)
+            #     # update the deal parameters last_inspection and last_inspection_date here
+            #     result = hubspot.deals.update_a_deal_oauth(dealId, {
+            #         'last_inspection': last_inspection_type.title(),
+            #         'last_inspection_date': hubspot_timestamp,
+            #         'insp_n': last_inspection_number})
+            #     if result:
+            #         print('Updated deal: ', dealId)
+            #         return True
+            # else:
+            #     print('did not update the note', id)
+            #     return False
+        else:
+            print('Is not ready')
+            return False
+
+    def prepare_note(self, **kwargs):
+        self.ready      = False
+        list_of_lines   = []
+
+        if 'companyId' in kwargs.keys():
+            self.companyId = kwargs['companyId']
+        if self.companyId:
+            self.deal_list = hubspot.associations.get_associations_oauth(self.companyId, '6')  # company to deals - full
+        else:
+            print('No company Id for the note. Nothing to prepare')
+        for deal_n in self.deal_list:
+            line = {}
+            deal_data = hubspot.deals.get_a_deal(deal_n)['properties']  # deal_n: int
+            # check the pipeline , dealstage , closedate , closed_won_reason , closed_lost_reason
+            pipeline = deal_data['pipeline']['value']
+            if pipeline == 'default':
+                dealname = deal_data['dealname']['value']
+                dealstage = deal_data['dealstage']['value']
+                dealstage_timestamp = deal_data['dealstage']['timestamp']
+                deal_owner = deal_data['hubspot_owner_id']['value']
+                # closed_won_reason = deal_data['closed_won_reason']['value']
+                # closed_lost_reason = deal_data['closed_lost_reason']['value']
+                if dealstage in hubspot.NAMES_OF_STATES.keys():
+                    stagename = hubspot.NAMES_OF_STATES[dealstage]
+                else:
+                    stagename = 'Deleted stage type'
+                if deal_owner in hubspot.OWNERS_OF_IDS.keys():
+                    ownername = hubspot.OWNERS_OF_IDS[deal_owner]
+                else:
+                    ownername = 'Deactivated user'
+
+                line.update(
+                    {'date': int(dealstage_timestamp), 'name': dealname, 'stage': stagename, 'owner': ownername})
+                list_of_lines.append(line)
+            else:
+                # pipeline is not 'default'
+                pass
+        # the list making cycle is over, time to format it
+        if list_of_lines:
+            to_sort = pd.DataFrame(list_of_lines)
+            to_publish = pd.DataFrame()
+            to_sort['date'] = pd.to_datetime(to_sort['date'], unit='ms')
+            to_publish = to_sort.sort_values(by=['date'], ascending=False)
+            to_publish['date'] = to_publish['date'].dt.strftime('%Y-%m-%d')
+            self.content = to_publish.to_html(index=False) # the html parameters: https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_html.html?highlight=to_html#pandas.DataFrame.to_html
+            self.ready = True
+
