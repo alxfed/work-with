2822a4ca9e2c6c0f00f4b9cc9bc36c4bac358359
alxfed <alxfed@gmail.com>
2019-12-25 11:00:07 -0600

    comparison

---
+++ compare_company_names.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""...
+"""
+import re
+import comparison
+
+
+def main():
+    name1 = 'D. & Sp. Construction Company'
+    name2 = 'S  & P Company Construction'
+    first_word_1 = comparison.compare.first_word(name1)
+    first_word_2 = comparison.compare.first_word(name2)
+    first_word_1 = re.sub('[ .]', '', first_word_1)
+    yes_or_no = comparison.compare.are_the_same(name=name1, comparable=name2, same=0.01)
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
+
+'''
+
+
+def main():
+    name1 = 'L & S Lighting Corp'
+    name2 = 'SoPf. & P. Tax Solutions '
+    name7 = 'S . & P Tax Solutions'
+    name3 = 'I.B. Quality. Cabinets'
+    name4 = 'CH& CP Kitchen Cabinet Gallery'
+    name5 = 'D.O.M.'
+    nname = 'one two'
+    c = constants.LIST_OF_JOINERS
+    a = set(name2)
+    b = set(name7)
+    jd = jaccard_dist(a, b)
+    pattern = re.compile(r'[&-/+\']')
+    # body = re.sub('>\s*<', '><', body, 0, re.M)
+    mate = re.match(r'(?:[.\w+])+', name2)
+    ma = mate.groups()
+    # ["](?P<protocol>http(?P<secure>s)?://)(?P<fqdn>[a-zA-Z0-9]*(?P<subdomain>(.)[a-zA-Z0-9]*)*)[/](?P<filename>([a-zA-Z.])*)["]
+    m = re.match(r'(?P<first>\w+) (?P<middle>\w+) (?P<last>\w+)', 'jane m doe')
+    di = m.groupdict()
+    ma = re.match(r"(?P<first>\w+) (?P<last>\w+)", nname)
+    m = ma.groupdict()
+    words = re.compile(r'(?P<first>\w+) (?P<second>\b\w+\b)')
+    mat = words.match(name2)
+    ma = mat.groupdict()
+    repl = pattern.sub(replace_match, name4, count=1)
+    rep = re.sub(r'[&-/+\']', '', name4)
+    # first = first_word(name2)
+    return
+
+
+if __name__ == '__main__':
+    main()
+    print('main - done')
+'''
--- comparison/compare.py
+++ comparison/compare.py
@@ -1,32 +1,7 @@
-from .constants import *
+from . import constants
 import re
 import nltk
-
-
-def first_word(name):
-    combinations = ['. ', ' ', '.']
-    sepp = ['&', '-', '/', '+', "'"]
-    name = name.strip(' .')
-    length = len(name)
-    no_spaces = length - len(name.replace(' ', ''))
-    no_periods = length - len(name.replace('.', ''))
-    no_commas = length - len(name.replace(',', ''))
-    no_dashes = length - len(name.replace('-', ''))
-    and_sign = length - len(name.replace('&', ''))
-    if not no_spaces == 0: # there are spaces to work with
-        one, sep, rest = name.partition(' ') # let's try the first split
-        if len(one) - len() == 0: # probably some abbreviation, maybe with dots
-            pass
-    else: # no_spaces == 0
-        if not no_periods == 0: # no spaces but some periods. A domain name?
-            one, sep, rest = name.partition('.')
-        else: # no spaces and no periods
-            if not no_dashes == 0:
-                one, sep, rest = name.partition('-')
-    repl = name.replace('. ', '', 1)
-    # if len(one) < 3:
-    #
-    return first_word
+from nltk.tokenize import RegexpTokenizer
 
 
 def replace_match(match):
@@ -46,40 +21,49 @@ def replace_match(match):
         string = ''
     return string
 
+
+def first_word(name):
+    # 1. length is less than 10
+    # 2. length is more than 10
+    frst_word = ''
+    tokenizer = RegexpTokenizer("\w+\.?\s+?[-/\+'&]\s+\w+\.?") # |\w+\s&' \w+&?\s+.\
+    tokens = tokenizer.tokenize(name)
+    if tokens:
+        frst_word = tokens[0]
+    return frst_word
+
+
+def are_the_same(name:str, comparable:str, same:float):
+    yes = False
+    return yes
+
+
 def jaccard_dist(a:set, b:set) -> float:
     jd = nltk.jaccard_distance(a, b)
     return jd
 
+'''
+    # combinations = ['. ', ' ', '.']
+    # sepp = ['&', '-', '/', '+', "'"]
+    # name = name.strip(' .')
+    # length = len(name)
+    # no_spaces = length - len(name.replace(' ', ''))
+    # no_periods = length - len(name.replace('.', ''))
+    # no_commas = length - len(name.replace(',', ''))
+    # no_dashes = length - len(name.replace('-', ''))
+    # and_sign = length - len(name.replace('&', ''))
+    # if not no_spaces == 0: # there are spaces to work with
+    #     one, sep, rest = name.partition(' ') # let's try the first split
+    #     if len(one) - len() == 0: # probably some abbreviation, maybe with dots
+    #         pass
+    # else: # no_spaces == 0
+    #     if not no_periods == 0: # no spaces but some periods. A domain name?
+    #         one, sep, rest = name.partition('.')
+    #     else: # no spaces and no periods
+    #         if not no_dashes == 0:
+    #             one, sep, rest = name.partition('-')
+    # repl = name.replace('. ', '', 1)
+    # if len(one) < 3:
+    #
 
-def main():
-    name1 = 'L & S Lighting Corp'
-    name2 = 'SoPf. & P. Tax Solutions '
-    name7 = 'S . & P Tax Solutions'
-    name3 = 'I.B. Quality. Cabinets'
-    name4 = 'CH& CP Kitchen Cabinet Gallery'
-    name5 = 'D.O.M.'
-    nname = 'one two'
-    a = set(name2)
-    b = set(name7)
-    jd = jaccard_dist(a, b)
-    pattern = re.compile(r'[&-/+\']')
-    # body = re.sub('>\s*<', '><', body, 0, re.M)
-    mate = re.match(r'(?:[.\w+])+', name2)
-    ma = mate.groups()
-    # ["](?P<protocol>http(?P<secure>s)?://)(?P<fqdn>[a-zA-Z0-9]*(?P<subdomain>(.)[a-zA-Z0-9]*)*)[/](?P<filename>([a-zA-Z.])*)["]
-    m = re.match(r'(?P<first>\w+) (?P<middle>\w+) (?P<last>\w+)', 'jane m doe')
-    di = m.groupdict()
-    ma = re.match(r"(?P<first>\w+) (?P<last>\w+)", nname)
-    m = ma.groupdict()
-    words = re.compile(r'(?P<first>\w+) (?P<second>\b\w+\b)')
-    mat = words.match(name2)
-    ma = mat.groupdict()
-    repl = pattern.sub(replace_match, name4, count=1)
-    rep = re.sub(r'[&-/+\']', '', name4)
-    # first = first_word(name2)
-    return
-
-
-if __name__ == '__main__':
-    main()
-    print('main - done')
+'''
