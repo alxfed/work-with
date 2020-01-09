dbefb31f513b47dddc7285daabd6b7fb5f6e3f53
alxfed <alxfed@gmail.com>
2020-01-09 08:12:07 -0600

    elgoog parameters on company

--- readin_all_companies_everything.py
+++ readin_all_companies_everything.py
@@ -41,29 +41,15 @@ def main():
 
     all_companies_cdr, all_columns = hubspot.companies.get_all_companies_oauth(request_params)
     all_companies = pd.DataFrame(all_companies_cdr, columns=all_columns)
+    # formatting
     all_companies.fillna(value='', inplace=True)
-    # all_companies['companyId'] = all_companies['companyId'].astype(dtype=object)
-    # all_companies['isDeleted'] = all_companies['isDeleted'].astype(dtype=bool)
-    # all_companies['name'] = all_companies['name'].astype(dtype=object)
-    # all_companies['phone'] = all_companies['phone'].astype(dtype=object)
-    # all_companies['address'] = all_companies['address'].astype(dtype=object)
-    # all_companies['city'] = all_companies['city'].astype(dtype=object)
-    # all_companies['zip'] = all_companies['zip'].astype(dtype=object)
-    # all_companies['state'] = all_companies['state'].astype(dtype=object)
-    # all_companies['category'] = all_companies['category'].astype(dtype=object)
-    # all_companies['domain'] = all_companies['domain'].astype(dtype=object)
-    # all_companies['website'] = all_companies['website'].astype(dtype=object)
-    # all_companies['summary_note_number'] = all_companies['summary_note_number'].astype(dtype=object)
-    # all_companies['summary_note_date_str'] = all_companies['summary_note_date_str'].astype(dtype=object)
-    # all_companies['elgoog_place_id'] = all_companies['elgoog_place_id'].astype(dtype=object)
-    # all_companies['elgoog_types'] = all_companies['elgoog_types'].astype(dtype=object)
     all_companies = all_companies.astype(dtype=object)
-
+    # store in home database
     conn = sqlalc.create_engine(sorting.HOME_DATABASE_URI)
     all_companies.to_sql(
         name=sorting.constants.COMPANIES_EVERYTHING_TABLE, con=conn,
         if_exists='replace', index=False)
-
+    # store in a file too
     with open(downuploaded_companies, 'w') as f:
         f_csv = csv.DictWriter(f, all_columns)
         f_csv.writeheader()
